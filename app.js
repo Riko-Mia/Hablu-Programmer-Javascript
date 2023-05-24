f2e0fdcbde8f1bde0a79cbdd7e942756cde9390b
Riko Mia <rikomia722@gmail.com>
2023-05-25 01:01:05 +0600

    25-5-2023 my computer

--- app.js
+++ app.js
@@ -1,5 +1,38 @@
 
-console.log('test ok')
+// string type 
+
+// var strType = 'hello riko'
+// console.log(strType, typeof strType)
+
+// // Number type  -27:40
+
+// var numType = 23
+// // var 
+// console.log(numType)
+
+// Boolean type - 33:40
+
+// var boolType = true
+// console.log(boolType, typeof boolType)
+
+// reserved key words _ 38:00
+
+// go to google and search javascript reserved key words 
+
+// to uppercase and to lowercase - 43:10
+
+// var uppercase = 'hello riko'
+// console.log(uppercase.toUpperCase())
+// console.log(uppercase.toLocaleLowerCase())
+
+
+
+
+
+
+
+
+
 
 
 
