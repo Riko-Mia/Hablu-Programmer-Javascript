b91baf1d3deadd64a88f2447f88538459e1fc19b
Riko Mia <rikomia722@gmail.com>
2023-05-27 23:22:02 +0600

    1 st assignment complet 27-05-23

--- app.js
+++ app.js
@@ -25,6 +25,51 @@
 // console.log(uppercase.toUpperCase())
 // console.log(uppercase.toLocaleLowerCase())
 
+// IndexOf() & Split() - 48:38
+
+// var myfd = 'riko, tutle, radit etc'
+// console.log(myfd.split('tutle'))
+// console.log(myfd.indexOf('etc'))
+
+
+// parseFloat() & parSeInt() - 59:19
+
+// var num1 = parseFloat('55'), num2 = parseInt('99.66') //99.66
+
+// console.log(num1 + num2)
+// console.log(typeof num2)
+// console.log(typeof (''+ 10))
+
+
+// JS operators - 1:11:00
+
+// let num1 = 50
+// let num2 = 30
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+// console.log(num1 * num2)
+// console.log(parseInt(num1 / num2))
+// console.log(num1 % num2)
+
+
+// Predefined Function - 1:20:47
+
+// let num1 = 50
+// let num2 = 30
+// console.log(Math.abs(-15))
+// console.log(Math.round(12.50))
+// console.log(Math.ceil(12.0001))
+// console.log(Math.floor(12.0001))
+// console.log(Math.floor(Math.random()*5) +1)
+
+
+// 1st Assignment - 1:37:10
+
+
+
+
+
+
 
 
 
